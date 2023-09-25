567919360aab6679de8a7f09d0ba6a2d7db984f5
Siddhartha Banerjee <siddhartha@hitthecode.de>
2023-09-25 16:32:04 +0200

    TIRE-89: Worked on the text rotator, added a marquee block

---
+++ blocks/rotator/rotator.js
@@ -0,0 +1,46 @@
+export default function decorate(block) {
+    const container = document.createElement('div');
+    container.style.display = "flex";
+    container.style.alignItems = "center";
+
+    const beforeText = document.createElement('span');
+    beforeText.textContent = "We are Hit The Code";
+    beforeText.style.marginRight = "10px";
+    beforeText.style.fontWeight = "bold";
+    const ul = document.createElement('ul');
+    ul.style.width = "80px";
+    ul.style.height = "50px";
+    ul.style.background = "#519d58";
+    ul.style.color = "white";
+    ul.style.listStyleType = "none";
+    ul.style.padding = "0";
+    ul.style.display = "flex";
+    ul.style.alignItems = "center";
+    ul.style.justifyContent = "center";
+
+    const afterText = document.createElement('span');
+    afterText.textContent = "Solution";
+    afterText.style.marginLeft = "10px";
+    afterText.style.fontWeight = "bold"; // Make the text bold
+
+    const li = document.createElement('li'); // Create a list item
+    li.textContent = "Find"; // Initial text content
+    ul.appendChild(li); // Add the list item to the ul
+
+    container.appendChild(beforeText);
+    container.appendChild(ul);
+    container.appendChild(afterText);
+    block.append(container);
+
+    const words = ["Find", "Build", "Deliver"];
+    let currentIndex = 0;
+
+    function updateText() {
+        li.textContent = words[currentIndex];
+        currentIndex = (currentIndex + 1) % words.length;
+    }
+
+    setInterval(updateText, 1000); // Set an interval to update the text every 1000ms (1 second)
+
+    updateText();
+}
