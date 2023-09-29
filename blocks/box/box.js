da7890a542266768bd1d17f22cf87d2184628f2c
Siddhartha Banerjee <siddhartha@hitthecode.de>
2023-09-29 16:56:19 +0200

    TIRE-89: new block to implement the icons in service page

---
+++ blocks/box/box.js
@@ -0,0 +1,34 @@
+import { createOptimizedPicture } from '../../scripts/lib-franklin.js';
+
+export default function decorate(block) {
+    const ul = document.createElement('ul');
+    ul.className = 'box';
+
+    [...block.children].forEach((row) => {
+        const li = document.createElement('li');
+        li.className = 'box-card';
+
+        const icon = document.createElement('i');
+        li.appendChild(icon);
+
+        while (row.firstElementChild) li.append(row.firstElementChild);
+        [...li.children].forEach((div) => {
+            if (div.children.length === 1 && div.querySelector('picture')) {
+                div.className = 'box-card-image';
+            } else {
+                div.className = 'box-card-body';
+            }
+        });
+
+        ul.append(li);
+    });
+
+    ul.querySelectorAll('.box-card-image img').forEach((img) => {
+        const picture = img.closest('picture');
+        picture.replaceWith(createOptimizedPicture(img.src, img.alt, false, [{ width: '750' }]));
+    });
+
+    block.textContent = '';
+    block.append(ul);
+}
+
