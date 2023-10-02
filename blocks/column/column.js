25cf66df603054b16b76a6da0650f6d3f23920eb
Siddhartha Banerjee <siddhartha@hitthecode.de>
2023-10-02 18:22:11 +0200

    TIRE-89: Updating the location with a map

--- blocks/column/column.js
+++ blocks/column/column.js
@@ -1,4 +1,9 @@
 export default function decorate(block) {
+    // Create a container element dynamically
+    const container = document.createElement('div');
+    container.classList.add('column-container'); // Add a class to the container for styling
+
+    // Your existing column setup code
     const cols = [...block.firstElementChild.children];
     block.classList.add(`column-${cols.length}-cols`);
 
@@ -15,4 +20,24 @@ export default function decorate(block) {
             }
         });
     });
+
+    // Create an iframe element for the map
+    const iframe = document.createElement('iframe');
+    iframe.src = `https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d10110.29127397713!2d10.9073126!3d50.6907539!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8ad587f15369341%3A0x77a0cbf69e2eca6!2shit%20the%20code%20GmbH!5e0!3m2!1sen!2sde!4v1696245424052!5m2!1sen!2sde`;
+    iframe.width = '100%'; // Set width
+    iframe.height = '400'; // Set height (you can adjust this as needed)
+    iframe.frameborder = '0'; // Remove border
+    iframe.allowfullscreen = ''; // Enable fullscreen mode
+    iframe.setAttribute('aria-hidden', 'false'); // Accessibility attribute
+    iframe.title = 'Google Map'; // Set a title for accessibility
+
+    // Append the iframe to the container
+    container.appendChild(iframe);
+
+    // Append the columns and map container to the block
+    cols.forEach((col) => {
+        container.appendChild(col);
+    });
+    block.textContent = '';
+    block.append(container);
 }
